59637f1c4ff009661283bdef4586d2fac02722a2
junaid hossain <junaidhossin50905@gmail.com>
2025-05-27 22:55:55 +0600

    Updated header for small device

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,65 +3,66 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <>
-            <header className="">
-                <nav className="navbar navbar-expand-sm bg-light">
-                    <div className="container-fluid">
-                        <NavLink className="navbar-brand" to={"/"}>
-                            <h3>Logo</h3>
-                        </NavLink>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div
-                            className="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            {/* left nav */}
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <NavLink
-                                        className="nav-link active"
-                                        aria-current="page"
-                                        to={"/discover"}
-                                    >
-                                        Discover
-                                    </NavLink>
-                                </li>
-                            </ul>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
 
-                            {/* right nav */}
-                            <ul className="navbar-nav ms-auto d-flex align-items-center gap-2">
-                                <li className="nav-item">
-                                    <Link
-                                        class="btn btn-dark rounded-pill px-4"
-                                        to={"/login"}
-                                    >
-                                        Login
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link
-                                        class="btn btn-primary rounded-pill px-4"
-                                        to={"/signup"}
-                                    >
-                                        Signup
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                    {/* Left: Burger menu */}
+                    <button
+                        className="navbar-toggler me-2"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Center: Logo */}
+                    <NavLink className="navbar-brand mx-auto" to="/">
+                        <h4 className="m-0">Logo</h4>
+                    </NavLink>
+
+                    {/* Right: Login & Signup */}
+                    <div className="d-flex d-lg-none gap-2">
+                        <Link className="btn btn-dark btn-sm rounded-pill px-3" to="/login">
+                            Login
+                        </Link>
+                        <Link className="btn btn-primary btn-sm rounded-pill px-3" to="/signup">
+                            Signup
+                        </Link>
                     </div>
-                </nav>
-            </header>
-        </>
+
+                    {/* Collapsible nav links (only visible when toggled) */}
+                    <div className="collapse navbar-collapse mt-3 mt-lg-0" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/discover">
+                                    Discover
+                                </NavLink>
+                            </li>
+                        </ul>
+
+                        {/* Right nav for large screens */}
+                        <ul className="navbar-nav ms-auto d-none d-lg-flex align-items-center gap-2">
+                            <li className="nav-item">
+                                <Link className="btn btn-dark rounded-pill px-4" to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="btn btn-primary rounded-pill px-4" to="/signup">
+                                    Signup
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
     );
 };
 
