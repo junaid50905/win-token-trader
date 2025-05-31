aa02cc78c2ee447ff27395634628ef333c60b1a0
junaid hossain <junaidhossin50905@gmail.com>
2025-05-31 12:16:49 +0600

    Updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer className="bg-dark mt-5 py-2">
             <div className="container-fluid">
                 <div className="row align-items-center">
-                    <div className="col-sm-8 col-12">
+                    <div className="col-md-8 col-sm-12 col-12">
                         <div className="d-flex gap-3 align-items-center">
                             <div className="logo">
                                 <Link to={"/"}>
@@ -28,15 +28,21 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-4 col-12">
-                        <div className="contact d-flex align-items-center gap-3">
+                    <div className="col-md-4 col-sm-12 col-12">
+                        <div className="contact d-flex align-items-center justify-content-sm-center justify-content-center gap-3">
                             <h6 className="text-white">Contact Us</h6>
                             <div className="icons d-flex align-items-center">
                                 <h3>
-                                    <TiSocialFacebook className="text-info" style={{cursor: 'pointer'}}/>
+                                    <TiSocialFacebook
+                                        className="text-info"
+                                        style={{ cursor: "pointer" }}
+                                    />
                                 </h3>
                                 <h3>
-                                    <SlSocialInstagram className="text-danger" style={{cursor: 'pointer'}}/>
+                                    <SlSocialInstagram
+                                        className="text-danger"
+                                        style={{ cursor: "pointer" }}
+                                    />
                                 </h3>
                             </div>
                         </div>
